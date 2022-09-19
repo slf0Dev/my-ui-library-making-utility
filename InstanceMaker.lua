cbdd66ff5cfbd359f72d7d0122957a5fdaad3004
slf0Dev <73026438+slf0Dev@users.noreply.github.com>
2022-09-20 04:33:54 +0700

    Update InstanceMaker.lua

--- InstanceMaker.lua
+++ InstanceMaker.lua
@@ -1,14 +1,20 @@
 local Module = {}
 
 function Module.Instance(instance : string,properties : table)
-    local CreatedInstance
-    local Corner
+    local CreatedInstance,Corner,Stroke
     CreatedInstance = Instance.new(instance)
 
     for property,value in next,properties do
-        if tostring(property) ~= "CornerRadius" then
+        if tostring(property) ~= "CornerRadius" or tostring(property) ~= "Stroke" then
             CreatedInstance[property] = value
-        else
+        elseif tostring(property) == "Stroke" then
+            Stroke = Instance.new("UIStroke",CreatedInstance)
+            Stroke.Color = value["Color"]
+            Stroke.Thickness = value["Thick"]
+            Stroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
+            Stroke.Transparency = value["Tranparency"]
+            Stroke.LineJoinMode = Enum.LineJoinMode.Round
+        elseif tostring(property) == "CornerRadius" then
             Corner = Instance.new("UICorner",CreatedInstance)
             Corner.CornerRadius = value
         end
